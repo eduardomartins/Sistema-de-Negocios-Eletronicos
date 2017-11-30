7f38203411e90bb34745055d8b276b382cd30bda
Eduardo Sant'Anna Martins <eduardo@eduardomartins.site>
2017-11-30 14:23:41 -0200

    Infra

--- infra.py
+++ infra.py
@@ -1,44 +1,64 @@
 # coding: utf-8
-from no import No, TAMANHOS
 from maquina import Estado
 
 
-
 class Firewall(Estado):
-    pass
-
+    def executar(self, pacote, interacao):
+        if interacao == 2:
+            pacote.atualiza_tempo(0.00015) # Adiciona 0,00015 seg
+        if interacao == 3:
+            pacote.atualiza_tempo(0.00015) # Adiciona 0,00015 seg
 
 
 class Roteador(Estado):
-    pass
-
+    def executar(self, pacote):
+        pacote.atualiza_tempo(tempo_sis=0.00015) # Adiciona 0,00015 seg
 
 
 class Lan(Estado):
+    capacidade = 0.8 * 0.125 # 0.125 MB/s equivale e 100Mbps
+    
     def executar(self, pacote):
-        pacote.atualiza_tempo(0.00015) # Adiciona 0,00015 seg
-
+        pacote.atualiza_tempo(tempo_sis=pacote.tamanho/self.capacidade) 
 
 
 class ServidorAplicacao(Estado):
-    def __init__(self, tempo_min, tempo_max, *args, **kwargs):
-        super(ServidorAplicacao, self).__init__(*args, **kwargs)
-        self.tempo_min = tempo_min
-        self.tempo_max = tempo_max
+    
+    def executar(self, pacote, interacao)
+        if interacao == 2:
+            capacidade = 1.0/(60 - 40)
+        
+        if interacao == 3:
+            scapacidade = 1.0/(120 - 60)
+
+        pacote.atualiza_tempo(tempo_sis=capacidade) 
+            
+
+class ServidorWeb(Estado):    
+    def executar(self, pacote, interacao, numero)
+    
+        if interacao == 1:
+            capacidade = 1.0/(6 - 4)
+    
+        if interacao == 2:
+            if numero == 2:
+                capacidade = 1.0/(7 - 5)
+            if numero == 5:
+                capacidade = 1.0/(10 - 7)
+        
+        if interacao == 3:
+            capacidade = 1.0/(9 - 12)
+        
+        pacote.atualiza_tempo(tempo_sis=capacidade) 
 
 
 
-class ServidorWeb(No, Estado):
-    def __init__(self, tempo_min, tempo_max, *args, **kwargs):
-        super(ServidorWeb, self).__init__(*args, **kwargs)
-        self.tempo_min = tempo_min
-        self.tempo_max = tempo_max
+class ServidorBancoDados(Estado):    
+    def executar(self, pacote)
+        capacidade = 1.0/(60 - 40) + 1/(400 - 50)
+        
+        pacote.atualiza_tempo(tempo_sis=capacidade) 
+        
 
 
-
-class ServidorBancoDados(No, Estado):
-    def __init__(self, tempo_min, tempo_max, *args, **kwargs):
-        super(ServidorBancoDados, self).__init__(*args, **kwargs)
-        self.tempo_min = tempo_min
-        self.tempo_max = tempo_max
 
