d82f8b0606e45de9c4d9db2dc80831cb9c06ab71
Eduardo Sant'Anna Martins <eduardo@eduardomartins.site>
2017-11-30 15:11:45 -0200

    Ajustes

--- main.py
+++ main.py
@@ -4,47 +4,44 @@
 """
 import time
 import sys
-from maquina import factory_maquina
-from infra import Lan, ServidorBancoDados, ServidorWeb, ServidorAplicacao, Roteador
+from maquina import Maquina
 from cliente import Cliente
+from no import trafego, TAMANHOS
+from infra import Lan, ServidorBancoDados, ServidorWeb, ServidorAplicacao, Roteador, Firewall
+
 
 DELAY = 0.0005
 VISITAS = 250
 
 
 def main(argv=sys.argv):
-
-    Interacao1 = factory_maquina('Interecao1')
-
-    Interacao2 = factory_maquina('Interecao2')
-
-    Interacao3 = factory_maquina('Interecao3')
-
-    roteador = Roteador()
-
-    interacao1 = Interacao1(roteador)
-    interacao2 = Interacao2(roteador)
-    interacao3 = Interacao3(roteador)
-
-    interacao1.inicial\
-        .add_proximo(ServidorWeb(100, 300))
-
-    interacao2.inicial\
-        .add_proximo(ServidorWeb(100, 300))\
-        .add_proximo(ServidorAplicacao(10, 20))\
-        .add_proximo(ServidorBancoDados(10, 20))\
-        .add_proximo(ServidorAplicacao(10, 20))
-
-    interacao3.inicial.add_proximo(ServidorWeb(100, 300))
-
+   
+    lan1 = Lan()
+    lan2 = Lan()
+    fw = Firewall()
+    rot = Roteador()
+    sw = ServidorWeb()
+    sa = ServidorAplicacao()
+    bd = ServidorBancoDados()
+    
+    maquina = Maquina(roteador)
+    no = maquina.inicial.add_proximo(Trafego(lan1)).add_proximo(Trafego(sw)).add_proximo(Trafego(sa, 0.95, TAMANHOS.get('M3')))
+    
+    # Interação 2
+    #interacao2 = 
+    no.add_proximo(Trafego(sw, 0.2, TAMANHOS.get('M3')))
+    
+    # Interação 3
+    #interacao3 =
+     no.add_proximo(Trafego(bd, 0.8, TAMANHOS.get('M6'))).add_proximo(Trafego(sa, 1, TAMANHOS.get('M7'))).add_proximo(Trafego(sw, 1, TAMANHOS.get('M9')))
+    
+                     
     cliente = Cliente()
-
-    cliente.register(interacao1)
+    cliente.register(maquina)
 
     pacotes = 0
     if len(argv) > 1:
         pacotes = int(argv[1])
-
 
     cliente.iniciar(pacotes, delay=DELAY)
 
